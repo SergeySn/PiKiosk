bb042309e4b56fbfd5cd4f602a58967d4a13916b
Abdur Rehman <abdur.rehman@linux.com>
2024-07-03 16:52:55 +0500

    sample-app: force 1920x1080 window size
    The sample app appears on a sub section of larger screens. Force
    resolution to 1920x1080 for now.
    
    Signed-off-by: Abdur Rehman <abdur.rehman@linux.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.window.WindowSize
+import androidx.compose.ui.unit.dp
 
 @Composable @Preview fun App()
 {
@@ -24,7 +27,9 @@ import androidx.compose.ui.window.application
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    val windowState = rememberWindowState(size = WindowSize(1920.dp, 1080.dp)) // Set this to match screen resolution
+
+    Window(onCloseRequest = ::exitApplication, state = windowState) {
         App()
     }
 }
