22f18f97bdbf413bfebd4eec98190fcfff64b564
Abdur Rehman <abdur.rehman@linux.com>
2024-07-05 12:50:09 +0500

    app: update ui to test keyboard input
    Simple UI with three components:
    1. Input TextField
    2. Button
    3. Output Text
    
    The user can enter text in Input TextField. When the Button is clicked,
    text is copied from Input TextField to Output Text.
    
    Signed-off-by: Abdur Rehman <abdur.rehman@linux.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,37 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 
 @Composable @Preview fun App()
 {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var inputText by remember { mutableStateOf("") }
+    var outputText by remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            TextField(
+                value = inputText,
+                onValueChange = { inputText = it },
+                label = { Text("Input") }
+            )
+            Button(onClick = { outputText = inputText }) {
+                Text("Copy Text")
+            }
+            Text("Output\n$outputText")
         }
     }
 }
